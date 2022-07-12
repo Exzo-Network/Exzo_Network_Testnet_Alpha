9951e2a17cfffaa2b57808c6cf95d13b18b2f60c
Exzo Network <89941175+Exzo-Network@users.noreply.github.com>
2022-07-12 17:53:05 -0400

    Update deploy_contract.js

--- web3/deploy_contract.js
+++ web3/deploy_contract.js
@@ -1,7 +1,7 @@
 const Web3 = require("web3");
 const utils = require("./compile_utils");
 
-const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+const web3 = new Web3(new Web3.providers.HttpProvider("http://143.198.51.224:8545"));
 
 async function getCurrentAccount() {
   const currentAccounts = await web3.eth.getAccounts();
